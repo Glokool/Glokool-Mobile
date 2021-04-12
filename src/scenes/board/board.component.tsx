9e1f32f976d8a02b91e8bd024ec9a92a2d5f3715
rhqjatjr0093 <rhqjatjr0093@gmail.com>
2021-04-12 14:58:05 +0900

    ios 테스트용 업로드

--- src/scenes/board/board.component.tsx
+++ src/scenes/board/board.component.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import auth from '@react-native-firebase/auth';
+import firestore from '@react-native-firebase/firestore';
 import { useFocusEffect } from '@react-navigation/native';
 import { BoardScreenProps } from '../../navigation/board.navigator'
 import {
@@ -12,9 +13,9 @@ import {
   ScrollView,
   BackHandler,
   Dimensions,
-  Linking
 } from 'react-native';
 import { 
+  Divider,
   IndexPath, 
   Layout, 
   LayoutElement,
@@ -22,11 +23,15 @@ import {
   SelectItem 
 } from '@ui-kitten/components';
 import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
-import { faSearch, faSyncAlt } from '@fortawesome/free-solid-svg-icons';
+import { faPencilAlt, faSearch, faSyncAlt, faThumbsDown, faThumbsUp } from '@fortawesome/free-solid-svg-icons';
+import moment from 'moment';
+import { SceneRoute } from '../../navigation/app.route';
 
 var ToastRef : any;
 var exitApp : any = undefined;
 var timeout : any;
+
+
 
 export const BoardScreen = (props: BoardScreenProps): LayoutElement => {
 
@@ -37,26 +42,81 @@ export const BoardScreen = (props: BoardScreenProps): LayoutElement => {
     ];
     const displayValue = BoardSelect[selectedBoard.row];
     const [refresh, setRefresh] = React.useState(false);
+    const [freeBoard, setFreeBoard] = React.useState([]);
+    const [qnaBoard, setQnaBoard] = React.useState([]);
 
-    const [freeBoard, setFreeBoard] = React.useState([{
-      title: '',
-      content: '',
-      writer: '',
-      writerAvatar: '',
-      writeDate: new Date(),
-      comment: [
-        {
-          writer: '',
-          writeDate: new Date(),
-          writerAvatar: ''
-        }
-      ]
-    }])
-    
+    // const [freeBoard, setFreeBoard] = React.useState([{
+    //   index: 0,
+    //   title: '',
+    //   content: '',
+    //   writer: '',
+    //   writerAvatar: '',
+    //   writeDate: new Date(),
+    //   plus: 0,
+    //   minus: 0,
+    //   comment: [
+    //     {
+    //       writer: '',
+    //       writeDate: new Date(),
+    //       writerAvatar: '',
+    //       content: ''
+    //     }
+    //   ]
+    // }]);
+    // const [qnaBoard, setQnaBoard] = React.useState([{
+    //   title: '',
+    //   content: '',
+    //   writer: '',
+    //   writerAvatar: '',
+    //   writeDate: new Date(),
+    //   plus: 0,
+    //   minus: 0,
+    //   comment: [
+    //     {
+    //       writer: '',
+    //       writeDate: new Date(),
+    //       writerAvatar: '',
+    //       content: ''
+    //     }
+    //   ]
+    // }]);
+
+    // 비동기 함수 실행을 위한 함수 분리
+    // 보드 데이터 다운로드
+    // 향후 바로 데이터로 교체할 것
+    async function downloadFreeBoard() {
+        const Free = await firestore().collection('FreeBoard').get();
+        const QnA = await firestore().collection('QnABoard').get();
+
+        var FreeBoard = [];
+
+        Free.forEach((doc) => {
+          
+          FreeBoard.push(doc.data());
+        });
+
+        setFreeBoard(FreeBoard.reverse()); 
+    }
+
+    async function downloadQnABoard() {
+        const QnA = await firestore().collection('QnABoard').get();
+        var QnABoard = [];
+        
+        QnA.forEach((doc) => {
+          QnABoard.push(doc);
+        });    
+
+        setQnaBoard(QnABoard.reverse());
+    }
 
     React.useEffect(() => {
         
-    }, [])
+        downloadFreeBoard();
+
+    }, []);
+
+
+   
 
     // 백핸들러 적용을 위한 함수
     const focusEvent = useFocusEffect(
@@ -94,7 +154,63 @@ export const BoardScreen = (props: BoardScreenProps): LayoutElement => {
     };
 
     const PressRefresh = () => {
+      if(displayValue === 'Free Board'){
+          downloadFreeBoard();
+      }
+      
+      else{
+          downloadQnABoard();
+      }
+    }
 
+    const PressPost = (item) => {
+      props.navigation.navigate(SceneRoute.BOARD_POST_DETAIL,
+        {
+          item: item, 
+          type: displayValue
+        });
+    }
+
+    const PressWrite = () => {
+      props.navigation.navigate(SceneRoute.BOARD_POST_CREATE);
+    }
+
+    const RenderBoard = (item) => {
+      
+      const day = moment(item.item.date).toDate();
+          
+      return(
+        <TouchableOpacity onPress={() => PressPost(item)}>
+        <Layout style={styles.post}>
+            
+            <Text style={styles.postTitle} numberOfLines={1}>
+              {item.item.title}
+            </Text>
+
+            <Text style={styles.postContent} numberOfLines={1}>
+              {item.item.content}
+            </Text>
+
+            <Layout style={styles.postFooter}>
+              
+              <Layout style={styles.footerSideContainer}>
+                <Text style={styles.postContent}>
+                    {`${day.getMonth() + 1}.${day.getDate()} | ${item.item.writer}`}
+                </Text>                
+              </Layout>
+
+              <Layout style={styles.footerSideContainer2}>
+                 <FontAwesomeIcon icon={faThumbsUp} size={10} color={'#02EA00'}/>
+                 <Text style={styles.iconNum}>{item.item.plus}</Text>
+                 <FontAwesomeIcon icon={faThumbsDown} size={10} color={'#FF4141'}/>
+                 <Text style={styles.iconNum}>{item.item.minus}</Text>
+              </Layout>              
+            </Layout>
+
+            <Divider style={{width: '100%', backgroundColor: 'gray', marginVertical: 5}}/>
+        </Layout>
+        </TouchableOpacity>
+      );
     }
 
 
@@ -123,20 +239,71 @@ export const BoardScreen = (props: BoardScreenProps): LayoutElement => {
                 </TouchableOpacity>
 
                 <TouchableOpacity style={styles.TouchIcon} onPress={() => PressSearch()}>
-                  <FontAwesomeIcon icon={faSearch} size={20}/>
+                  <FontAwesomeIcon icon={faSearch} size={20} />
                 </TouchableOpacity>
 
               </Layout>
            </Layout>
 
            {/* 게시판 내용 표기 Flat List */}
+           
+               
+          {(displayValue === 'Free Board')? 
+            <Layout style={styles.BoardContainer}>
+              
+              {(freeBoard.length != 0)?
+                <FlatList
+                  data={freeBoard}
+                  style={{backgroundColor: 'white', width: '100%'}}
+                  contentContainerStyle={{paddingBottom: 300}}
+                  renderItem={RenderBoard}
+                  keyExtractor={item => item.index}
+                />
+              :
+              
+                <Text style={{fontSize: 16, color: 'gray', fontWeight: 'bold', textAlign: 'center'}}>There are no posts here yet. Why don't you try one?</Text>
 
-           <Layout style={styles.BoardContainer}>
+              }
+              
+            </Layout>        
+          : 
+            null
+          }
 
-           </Layout>
+          {(displayValue === 'QnA Board')? 
+            
+            <Layout style={styles.BoardContainer}>
+              {(qnaBoard.length != 0)?
+                  <FlatList
+                  data={qnaBoard}
+                  style={{backgroundColor: 'white', width: '100%'}}
+                  contentContainerStyle={{paddingBottom: 300}}
+                  renderItem={RenderBoard}
+                  keyExtractor={item => item.index}
+                  />
+
+                :
+                
+                <Text style={{fontSize: 16, color: 'gray', fontWeight: 'bold', textAlign: 'center'}}>There are no posts here yet. Why don't you try one?</Text>
+
+            
+              }
+              
+            </Layout>           
+          : 
+            null
+          }
+               
+
            
 
-        
+           {/* 커스텀 바텀 바 (글쓰기 버튼) */}
+           <TouchableOpacity style={styles.customBottomBar} onPress={() => PressWrite()}>
+                <FontAwesomeIcon icon={faPencilAlt} size={16} color={'#FFD774'} style={{marginRight: 15}}/>
+                <Text style={{fontSize: 16, fontWeight: 'bold', color: '#9C9C9C'}}>Writing</Text>
+           </TouchableOpacity>
+           
+
 
 
        </React.Fragment>
@@ -168,10 +335,60 @@ const styles = StyleSheet.create({
   BoardSelect: {
     width: '100%',
     justifyContent: 'center',
-    alignItems: 'center'
+    alignItems: 'center',
+    backgroundColor: 'white'
   },
   BoardContainer: {
-    flex: 9
+    flex: 9,
+    alignItems: 'center',
+    justifyContent: 'center'
+  },
+  post: {
+    width: '100%',
+    height: undefined,
+    padding: 10,
+  },
+  postTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+    color: 'black'
+  },
+  postContent: {
+    fontSize: 12,
+    color: 'gray',
+    marginVertical: 5  
+  },
+  postFooter: {
+    flexDirection: 'row',
+  },
+  footerSideContainer: {
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+    flex: 1
+  },
+  footerSideContainer2: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  iconNum: {
+    marginLeft: 2,
+    marginRight: 5
+  },
+  customBottomBar: {
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 25,
+    left: '32.5%',
+    borderRadius: 15,
+    width: '35%',
+    height: '5%',
+    backgroundColor: '#C9C9C9',
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderColor: '#9C9C9C',
+    borderWidth: 0.5
   }
 
 
--- src/scenes/board/post.detail.component.tsx
+++ src/scenes/board/post.detail.component.tsx
@@ -17,10 +17,16 @@ import {
   Layout,
   LayoutElement,
 } from '@ui-kitten/components';
+import { faAngleLeft, faExclamationTriangle } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome';
 
 var ToastRef : any;
 
-export const PostDetailScreen = (props: PostDetailScreenProps ): LayoutElement => {
+export const PostDetailScreen = (props: PostDetailScreenProps): LayoutElement => {
+    
+    const data = props.route.params;
+
+    console.log(data);
 
 
     React.useEffect(() => {
@@ -28,12 +34,87 @@ export const PostDetailScreen = (props: PostDetailScreenProps ): LayoutElement =
     }, [])
 
 
+    const PressBack = () => {
+      props.navigation.goBack()
+    }
+
+    const PressReport = () => {
+
+    }
+
+
 
     return(
        <React.Fragment>
+
+         {/* top Tab bar*/}
+        <Layout style={styles.TopTabBar}>
+          
+          <TouchableOpacity style={styles.leftIcon}>
+            <FontAwesomeIcon icon={faAngleLeft} size={32}/>
+          </TouchableOpacity>
+
+          <TouchableOpacity style={styles.rightIcon}>
+            <FontAwesomeIcon icon={faExclamationTriangle} size={30}/>
+          </TouchableOpacity>
+        </Layout>
+
+        {/* 메인 컨텐츠 컨테이너 */}
+
+        <Layout style={styles.mainContainer}>
+          
+          <Layout style={{flex: 1}}/>
+          
+          <Layout style={styles.profileContainer}>
+            <Image style={styles.profileImage} source={require('../../assets/profile/profile_01.png')}/>
+          </Layout>
+        </Layout>
+
+ 
+
 
 
 
        </React.Fragment>
     );
 }
+
+const styles = StyleSheet.create({
+  TopTabBar: {
+    flex: 1,
+    flexDirection: 'row',
+    padding: 10,
+    position: 'absolute',
+    top: 0,
+    width: '100%',
+    zIndex: 10,
+    backgroundColor: 'white'
+  },
+  leftIcon: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'flex-start'
+  },
+  rightIcon: {
+    flex: 1,
+    padding: 10,
+    justifyContent: 'center',
+    alignItems: 'flex-end'
+  },
+  mainContainer: {
+    flex: 9,
+    zIndex:9,
+    backgroundColor: 'white'
+  },
+  profileContainer: {
+    flexDirection: 'row',
+    flex: 1,
+    padding: 30
+  },
+  profileImage: {
+    width: 32,
+    height: 32,
+    resizeMode: 'stretch'
+  }
+})
