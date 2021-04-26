736eae4d998a6b897a2f3fad8c8de421f6da3d82
rhqjatjr0093 <rhqjatjr0093@gmail.com>
2021-04-26 13:55:07 +0900

    홈 화면 구성중 2

--- src/scenes/home/home.component.tsx
+++ src/scenes/home/home.component.tsx
@@ -7,6 +7,7 @@ import { SafeAreaView } from "react-native-safe-area-context"
 import { FlatList } from "react-native-gesture-handler"
 import axios from "axios"
 import { SERVER } from "../../server.component"
+import auth from '@react-native-firebase/auth';
 import { PageIndicatorConfig } from "react-native-banner-carousel/out/Carousel"
 import { NavigatorRoute, SceneRoute } from "../../navigation/app.route"
 
@@ -18,7 +19,7 @@ export const HomeScreen = (props: HomeScreenProps): LayoutElement => {
 
   const [content, setContent] = React.useState([]);
   const [tour, setTour] = React.useState([]);
-  const BannerWidth = Dimensions.get('window').width;
+  const BannerWidth = Dimensions.get('window').width * 0.8;
   const BannerHeight = 110;
 
   const banner = [
@@ -74,13 +75,13 @@ export const HomeScreen = (props: HomeScreenProps): LayoutElement => {
     }
 
     return(
-      <Layout style={{ padding: 10, borderWidth: 2, borderColor: 'red' ,justifyContent: 'center', alignItems: 'center'}}>
+      <Layout style={{ padding: 10, justifyContent: 'center', alignItems: 'center'}}>
         
         <TouchableOpacity onPress={() => PressTour(item.id)}>
           <Image source={{ uri : item.banner}} style={{ width: 250, height: 330, resizeMode: 'stretch', borderRadius: 5 }}/>
         </TouchableOpacity>
 
-        <Layout style={{ position: 'absolute', alignItems: 'center', justifyContent: 'center',bottom: 10, backgroundColor: '#00FF0000'}}>
+        <Layout style={{ position: 'absolute', alignItems: 'center', justifyContent: 'center',bottom: 20, backgroundColor: '#00FF0000'}}>
           <Text style={{ fontSize: 25, fontFamily: 'BrandonGrotesque-Black', color: 'white' }}>{item.title}</Text>
         </Layout>
 
@@ -107,19 +108,75 @@ export const HomeScreen = (props: HomeScreenProps): LayoutElement => {
 
 
   return(
-    <ScrollView>
-      <SafeAreaView style={{ flex: 0, backgroundColor: 'white'}} />
+    <Layout>
 
-        {/* 투어 추천 */}
-        <Layout style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', paddingHorizontal: 30, marginVertical: 10 }}>
+      <ScrollView
+        showsVerticalScrollIndicator={false}
+      >
+        <SafeAreaView style={{ flex: 0, backgroundColor: 'white'}} />
 
-          <Layout style={{ flex: 1 }}>
-            <Text style={{ fontSize: 25, fontFamily: 'BrandonGrotesque-Black' }}>Recommend Tour</Text>
+        <Layout style={{ width: '100%', height: 70,}} />
+
+          {/* 투어 추천 */}
+          <Layout style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', paddingHorizontal: 30, marginVertical: 10 }}>
+
+            <Layout style={{ flex: 1 }}>
+              <Text style={{ fontSize: 25, fontFamily: 'BrandonGrotesque-Black' }}>Recommend Tour</Text>
+            </Layout>
+
+            <TouchableOpacity style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'flex-end'}} onPress={() => props.navigation.navigate('FEED') }>
+              <Text style={{ fontSize: 15, fontFamily: 'BrandonGrotesque-Black', color: '#FFD878' }}>SEE MORE</Text>
+            </TouchableOpacity>      
+
           </Layout>
 
-          <TouchableOpacity style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'flex-end'}} onPress={() => props.navigation.navigate('FEED') }>
+          <Layout style={{ alignItems: 'center', justifyContent: 'center' }}>
+
+            <FlatList
+              style={{backgroundColor: '#00FF0000'}}
+              data={tour}
+              renderItem={renderTour}
+              horizontal={true}
+              
+              keyExtractor={item => item.id}
+              getItemLayout={(data, index) => (
+                {length: 350, offset: 270 * index, index}
+              )}
+              initialScrollIndex={0.75}
+            />
+
+          </Layout>
+
+
+          {/* 캐러셀 */}
+          <Layout style={styles.mainContainer}>
+            <Carousel
+                autoplay
+                autoplayTimeout={5000}
+                loop
+                index={0}
+                pageSize={BannerWidth}
+            >
+              {(banner.map((item) =>   
+                <TouchableOpacity onPress={() => {Linking.openURL(item.url)}} style={styles.banner}>
+                  <Image style={{width: BannerWidth, height: BannerHeight, resizeMode: 'stretch', borderRadius: 5}} source={item.image}/>
+                </TouchableOpacity>
+              ))}
+                
+            </Carousel>
+
+          </Layout>
+
+        {/* 컨텐츠 */}
+        <Layout style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', paddingHorizontal: 30, marginVertical: 10 }}>
+
+          <Layout style={{ flex: 1, }}>
+            <Text style={{ fontSize: 25, fontFamily: 'BrandonGrotesque-Black' }}>Contents</Text>
+          </Layout>
+
+          <TouchableOpacity style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'flex-end'}}>
             <Text style={{ fontSize: 15, fontFamily: 'BrandonGrotesque-Black', color: '#FFD878' }}>SEE MORE</Text>
-          </TouchableOpacity>      
+          </TouchableOpacity>       
 
         </Layout>
 
@@ -127,66 +184,40 @@ export const HomeScreen = (props: HomeScreenProps): LayoutElement => {
 
           <FlatList
             style={{backgroundColor: '#00FF0000'}}
-            data={tour}
-            renderItem={renderTour}
-            horizontal={true}
-            
+            data={content}
+            renderItem={renderContent}
             keyExtractor={item => item.id}
-            getItemLayout={(data, index) => (
-              {length: 350, offset: 270 * index, index}
-            )}
-            initialScrollIndex={0.75}
+            numColumns={2}
           />
 
         </Layout>
 
 
-        {/* 캐러셀 */}
-        <Layout style={styles.mainContainer}>
-          <Carousel
-              autoplay
-              autoplayTimeout={5000}
-              loop
-              index={0}
-              pageSize={BannerWidth}
-          >
-            {(banner.map((item) =>   
-              <TouchableOpacity onPress={() => {Linking.openURL(item.url)}} style={styles.banner}>
-                <Image style={{width: BannerWidth, height: BannerHeight, resizeMode: 'stretch', borderRadius: 5}} source={item.image}/>
-              </TouchableOpacity>
-            ))}
-              
-          </Carousel>
 
-        </Layout>
+      </ScrollView>
+      
+      <Layout style={{ position: 'absolute', top: 0, width: '100%', height: 70, alignItems: 'center', padding: 20, flexDirection: 'row'}}>
 
-      {/* 컨텐츠 */}
-      <Layout style={{ flexDirection: 'row', alignItems: 'center', justifyContent: 'center', paddingHorizontal: 30, marginVertical: 10 }}>
-
-        <Layout style={{ flex: 1, }}>
-          <Text style={{ fontSize: 25, fontFamily: 'BrandonGrotesque-Black' }}>Contents</Text>
-        </Layout>
-
-        <TouchableOpacity style={{ flex: 1, justifyContent: 'flex-end', alignItems: 'flex-end'}}>
-          <Text style={{ fontSize: 15, fontFamily: 'BrandonGrotesque-Black', color: '#FFD878' }}>SEE MORE</Text>
-        </TouchableOpacity>       
-
-      </Layout>
-
-      <Layout style={{ alignItems: 'center', justifyContent: 'center' }}>
-
-        <FlatList
-          style={{backgroundColor: '#00FF0000'}}
-          data={content}
-          renderItem={renderContent}
-          keyExtractor={item => item.id}
-          numColumns={2}
-        />
+          <Layout style={{ flex: 1 }}>
+            <Image source={require('../../assets/glokoolLogo.png')} />
+          </Layout>
+          
+          <TouchableOpacity onPress={() => props.navigation.navigate('MY PAGE')}>          
+              {(auth().currentUser)?
+                (auth().currentUser?.photoURL != '')?
+                <Image source={{uri : auth().currentUser?.photoURL}} style={{ width: 34, height: 34, borderRadius: 50 }}/>
+                :
+                <Image source={require('../../assets/profile/profile_05.png')} style={{ width: 34, height: 34 }}/>
+              :
+                <Image source={require('../../assets/profile/profile_06.png')} style={{ width: 34, height: 34 }}/>
+              }
+          </TouchableOpacity>
 
       </Layout>
 
 
-    </ScrollView>
+    </Layout>
+
   )
 }
 
@@ -197,12 +228,11 @@ const styles = StyleSheet.create({
     backgroundColor: '#FFFFFF',
     borderRadius: 15,
     height: 120,
-    borderColor: '#FFD774',
-    borderWidth: 2
   },
   banner: {
     justifyContent: 'center',
     alignItems: 'center',
     borderRadius: 15,
+    width: Dimensions.get('window').width * 0.8,
   },
 });
