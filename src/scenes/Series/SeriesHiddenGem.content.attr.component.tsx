87a6c9ef0d9bd7687877e111aac168416d12382d
trumanfromkorea <jangjh971008@naver.com>
2021-08-04 15:39:32 +0900

    가이드북 glo chat service 선 제거

--- src/scenes/Series/SeriesHiddenGem.content.attr.component.tsx
+++ src/scenes/Series/SeriesHiddenGem.content.attr.component.tsx
@@ -561,14 +561,14 @@ const styles = StyleSheet.create({
         marginBottom: 10,
     },
     GlochatContainer: {
-        shadowColor: '#000',
-        shadowOffset: {
-            width: 0,
-            height: 1,
-        },
-        shadowOpacity: 0.18,
-        shadowRadius: 1.0,
-        elevation: 1,
+        // shadowColor: '#000',
+        // shadowOffset: {
+        //     width: 0,
+        //     height: 1,
+        // },
+        // shadowOpacity: 0.18,
+        // shadowRadius: 1.0,
+        // elevation: 1,
         marginBottom: 20,
         paddingTop: 15,
         paddingBottom: 15,
