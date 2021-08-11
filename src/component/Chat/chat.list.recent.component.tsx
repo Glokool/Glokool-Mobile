4c6853f3ffff03cea5903760c56ce55f1bef80ac
trumanfromkorea <jangjh971008@naver.com>
2021-08-11 14:55:49 +0900

    travel plan,area 추가

--- src/component/Chat/chat.list.recent.component.tsx
+++ src/component/Chat/chat.list.recent.component.tsx
@@ -228,14 +228,6 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         marginBottom: 26,
-        // shadowColor: '#000',
-        // shadowOffset: {
-        //     width: 0,
-        //     height: 1,
-        // },
-        // shadowOpacity: 0.2,
-        // shadowRadius: 1.41,
-        // elevation: 2,
     },
     EmptyButtonText: {
         fontFamily: 'BrandonGrotesque-BoldItalic',
