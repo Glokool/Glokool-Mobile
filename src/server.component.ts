9e24846be65dba5065e6e5106f6d9fdcbc308756
trumanfromkorea <jangjh971008@naver.com>
2021-09-14 10:34:20 +0900

    refactor: initial CDN

--- src/server.component.ts
+++ src/server.component.ts
@@ -1,12 +1,13 @@
 let serverAPI;
 
 if (process.env.NODE_ENV === "development"){
-    serverAPI = 'https://glokool.info';
+    serverAPI = 'http://192.168.35.204:4000';
 } else {
     serverAPI = 'https://glokool.info';
 }
 
 export const SERVER = serverAPI;
+export const CDN = 'https://img.glokool-guide.com';
 
 // export const SERVER = 'https://glokool.info'; // 배포 서버
 // export const SERVER = 'https://glokool-guide.com'; // 테스트 서버 <- 배포 서버 copy
