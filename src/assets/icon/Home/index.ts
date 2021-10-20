c0f001b8b1032a68a76a5d2a169ff5c2871088df
trumanfromkorea <jangjh971008@naver.com>
2021-10-20 15:47:59 +0900

    add: home scene 디자인 적용

--- src/assets/icon/Home/index.ts
+++ src/assets/icon/Home/index.ts
@@ -1,8 +1,15 @@
-
+import EmptyHomeImage from './EmptyHomeImage.svg';
 import TextLogo from './TextLogo.svg';
 import LoginIcon from './LoginIcon.svg';
+import GloChatHomeButton from './GloChatHomeButton.svg';
+import GloChatInfoButton from './GloChatInfoButton.svg';
+import HomeGloIcon from './HomeGloIcon.svg';
 
 export {
     TextLogo,
     LoginIcon,
+    EmptyHomeImage,
+    GloChatHomeButton,
+    GloChatInfoButton,
+    HomeGloIcon,
 }
