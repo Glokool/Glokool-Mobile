14483c11443321e40a90f8c821e5595071532307
trumanfromkorea <jangjh971008@naver.com>
2021-11-10 14:11:48 +0900

    fix: chat room padding 조정

--- src/scenes/Chat/Chat.Room.scene.tsx
+++ src/scenes/Chat/Chat.Room.scene.tsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { ChatComponent } from '../../component/Chat/ChatRoom';
 import { ChatRoomSceneProps } from '../../navigation/SceneNavigator/Chat.navigator';
 
-
 export const ChatRoomScene = (props: ChatRoomSceneProps): React.ReactElement => {
 
     return (
